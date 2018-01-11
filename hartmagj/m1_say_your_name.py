b5558b5a472c9a1665b2d19591e693f1ded8e43e
hartmagj <hartmagj@rose-hulman.edu>
2018-01-11 16:32:48 -0500

    time cube conversion underway

--- hartmagj/m1_say_your_name.py
+++ hartmagj/m1_say_your_name.py
@@ -22,7 +22,518 @@ import ev3dev.ev3 as ev3
 
 
 def main():
-    ev3.Sound.speak("Nothing on Earth more Evil than a human educated as 1, when composed of opposites that cancel out as an entity.").wait(20)
+    ev3.Sound.speak('In 1884,  meridian time personnel met
+    ev3.Sound.speak("    in Washington to change Earth time.
+    ev3.Sound.speak('First words said was that only 1 day
+    ev3.Sound.speak('could be used on Earth to not change
+
+    ev3.Sound.speak('    the 1 day bible. So they applied the 1
+
+    ev3.Sound.speak('day  and  ignored  the  other  3 days.
+
+    ev3.Sound.speak('The bible time was wrong then and it
+
+    ev3.Sound.speak('    proved wrong today. This a major lie
+
+    ev3.Sound.speak('    has so much evil feed from it's wrong.
+
+    ev3.Sound.speak('No man on Earth has no belly-button,
+
+    ev3.Sound.speak('    it proves every believer on Earth a liar.
+
+    ev3.Sound.speak('Children will be blessed for
+
+    ev3.Sound.speak('Killing Of Educated Adults
+
+    ev3.Sound.speak('Who Ignore 4 Simultaneous
+
+    ev3.Sound.speak('    Days Same Earth Rotation.
+
+    ev3.Sound.speak('Practicing  Evil  ONEness -
+
+    ev3.Sound.speak('Upon Earth Of  Quadrants.
+
+    ev3.Sound.speak('    Evil Adult Crime VS Youth.
+
+    ev3.Sound.speak('    Supports Lie Of Integration.
+
+    ev3.Sound.speak('    1 Educated Are Most Dumb.
+
+  Not 1 Human Except Dead 1.
+
+  Man Is Paired,  2 Half 4 Self.
+
+  1 of God Is Only 1/4 Of God.
+
+   Bible A Lie & Word Is Lies.
+
+   Navel Connects 4 Corner 4s.
+
+  God Is Born Of A Mother –
+
+   She Left Belly B. Signature.
+
+Every Priest Has Ma Sign
+
+  But Lies To Honor Queers.
+
+Belly B. Proves 4 Corners.
+
+
+
+Your dirty lying teachers
+
+use only the midnight to
+
+midnight 1 day (ignoring
+
+3 other days) Time to not
+
+foul (already wrong) bible
+
+   time. Lie that corrupts earth
+
+you educated stupid fools.
+
+
+
+GoBelly-Button  Logic Works.
+
+
+
+When   Do  Teenagers  Die?
+
+Adults Eat Teenagers Alive,
+
+ No Record  Of  Their Death.
+
+  Father Son Image, Not Gods.
+
+ Every Man Born Of Woman.
+
+
+
+
+
+Belly-Button Is the Signature
+
+Of  Your Personal Creator -
+
+I Believe Her Name Mama.
+
+
+
+Pastor Told His Flock That
+
+God Created All Of Them -
+
+   Truth Was That They All had
+
+Mama Made Belly Buttons,
+
+Church Was Full Of Liars.
+
+
+
+       Earth Has 4 Days In Same 24      Hrs., 1 Day God Was Wrong.
+
+Einstein  Was  ONEist  Brain.
+
+Try  My  Belly-Button  Logic.
+
+      No God Knows About 4 Days,     It  Is  Evil  To  Ignore 4 Days,
+
+Does Your Teacher Know ?
+
+
+
+Fraudulent ONEness of religious
+
+academia has retarded your opposite
+
+rationale brain to a half brain slave.
+
+YOU IGNORE 3 OF 4 DAYS -
+
+FORCE 4 DAYS ON EARTH,
+
+THEY ALREADY EXIST.
+
+4 HORSEMEN HAVE 4 DAYS
+
+IN ONLY 1 EARTH ROTATION.
+
+4 ANGLES STOOD ON 4 CORNERS.
+
+4 CORNERS ROTATE TO 16 CORNERS
+
+WHICH EQUAL TO 4 CORNER DAYS.
+
+TEACHERS ARE EVIL LIARS - THE
+
+ONEness OF GOD IS STILLness DEATH.
+
+YOU WERE ONEness RETARD ON THE
+EARTH OPPOSITES ALL YOUR  LIFE.
+LOVE OF GOD IS HATE OF CHILDREN.
+SUPPORT TIMECUBE OR BE CURSED.
+EARTH HAS 4 CORNER
+
+SIMULTANEOUS 4-DAY
+
+TIME CUBE
+
+WITHIN SINGLE ROTATION.
+
+ 4 CORNER DAYS PROVES 1
+
+DAY 1 GOD IS TAUGHT EVIL.
+
+IGNORANCE OF TIMECUBE4
+
+SIMPLE MATH IS RETARDATION
+
+AND EVIL EDUCATION DAMNATION.
+
+CUBELESS AMERICANS DESERVE -
+
+AND SHALL BE EXTERMINATED.
+
+******************************************************************************
+
+The ONLY  Official Site
+For Gene Ray/TimeCube.
+Need Help - Donate to Timecube/Gene Ray at PayPal
+
+******************************************************************************
+I am a Knower of 4 corner
+simultaneous 24 hour Days
+that occur within a single
+4 corner rotation of Earth.
+*********************************************************************
+
+God guise for Queer
+scam, enslaves 4 Day
+
+cube brain as  ONEist.
+
+Vilify teachers - for
+
+Queers swindle Tithe
+
+from 1 Day Retarded
+
+***********************************************************************
+
+Till You KNOW 4 Simultaneous Days
+Rotate In Same 24 Hours Of Earth
+You Don't Deserve To Live On Earth
+
+
+
+Americans are actually RETARDED from
+
+ Religious Academia taught ONEism -upon
+
+an Earth of opposite poles, covered by Mama
+
+Hole and Papa Pole pulsating opposite sexes.
+
+The ONEist educated with their flawed 1 eye
+
+perspective (opposite eyes overlay) Cyclops
+
+ mentality, inflicts static non pulsating logos
+
+as a fictitious queer same sex transformation.
+
+**********************************
+
+It Is The Absolute Verifiable Truth & Proven Fact
+That Your Belly-Button Signature Ties
+To Viviparous Mama.
+
+**********************************
+
+Life is pulsing opposite mirror Pairs,
+
+Death is ONEness of Godism.
+
+****************************************************************************************
+
+ONE - DOES NOT EXIST,
+
+EXCEPT IN DEATH STATE.
+ ONE IS A DEMONIC RELIGIOUS LIE.
+*************************************
+Until you can tear and burn the bible to
+escape the EVIL ONE, it will be impossible
+for your educated stupid brain to know that
+4 different corner harmonic 24 hour Days
+rotate simultaneously within a single 4
+quadrant rotation of a squared equator
+and cubed Earth. The Solar system, the
+Universe, the Earth and all humans are
+composed of + 0 - antipodes, and equal
+to nothing if added as a ONE or Entity.
+All Creation occurs between Opposites.
+Academic ONEism destroys  +0- brain.
+If you would acknowledge
+simple existing math proof
+that 4 harmonic corner days
+rotate simultaneously around
+squared equator and cubed
+Earth, proving 4 Days, Not
+1Day,1Self,1Earth or 1God
+that exists only as anti-side.
+This page you see - cannot
+exist without its anti-side
+existence, as +0- antipodes.
+Add +0- as One = nothing.
+**********************************************************************
+Seek Awesome Lectures,
+MY WISDOM DEBUNKS GODS OF
+ ALL RELIGIONS AND ACADEMIA.
+
+We have a Major Problem,
+Creation is Cubic Opposites,
+2 Major Corners & 2 Minor.
+Mom/Dad & Son/Daughter,
+NOT taught Evil ONEism,
+which  VOIDS Families.
+******************************************************************************************
+Seek Wisdom of Cubic Life
+Intelligence - or you die evil.
+****************************************************************************************
+ Evil God Believers refuse to
+acknowledge  4 corner Days
+rotating simultaneously around
+4 quadrant created Earth -
+in only 1 rotation, voiding the
+Oneism Evil  1 Day 1 God.
+You worship Satanic impostor
+guised by educators as 1 god.
+
+No 1 God equals 4 - 24 hour
+Days Rotating Simultaneously
+within 1- 24 hour Rotation of
+ 4 quadrant created Earth.
+Ignoring 4 Corner Earth Days
+will Destroy Evil Humanity.
+I am organizing Children to
+join "Cubic Army of 4 Days"
+to convert Evil 1 Day Adults
+to 4 Day mentality existence,
+to serve perpetual humanity.
+
+"Nothing on Earth more Evil
+than a human educated as 1,
+when composed of opposites
+that cancel out as an entity."
+In fact, man is the only 1 Evil,
+and will soon erase himself by
+ignoring Cubic 4 Day Creation.
+If a Man cannot tear a page
+from the bible and burn it -
+then he cannot be a scientist,
+or participate in Symposium -
+to measure Cubing of Earth
+with Cubic intelligence wiser
+than any man or god known.
+Educators have destroyed the
+human analytical brain to a
+single perspective, in spite of
+all creation within  Universe
+being based upon opposites,
+binaries & antipodes, including
+Sun/Earth binary relative to the
+
+ human male/female binary. No
+ ancient insignificant dead 1 Jew
+ godism can match or exceed the
+enormity of the Sun/Earth
+Binary. His heart is not big
+enough for sharing with the
+vastness of created opposites.
+1 has no heart beat or breath,
+constituting death of opposites.
+God in Human form has human
+
+ limits as body controls activity.
+You are taught Evil, You act
+Evil, You are the Evil on Earth.
+Only your comprehending the
+Divinity of Cubic Creation will
+your soul be saved from your
+created hell on Earth - induced
+by your ignoring the existing
+4 corner harmonic simultaneous
+ 4 Days rotating in a single cycle
+of the Earth sphere. Religious/
+Academic Pedants cannot allow
+4 Days that contradict 1Day 1God.
+Educators destroy your brain,
+but you don't know, so why care?
+
+Creation ocurrs via opposites,
+but Religious/Academia pedants
+suppress it teaching Satanic One.
+
+After 30 years of research, I now
+possess the Order of Harmonic
+Antipodal Cubic Divinity Life -
+too large for physical form, but
+Binary Spirit of the masculinity
+Sun & feminity Earth Antipodes.
+ONEism is demonic Death Math.
+I have so much to teach you, but
+you ignore me you evil asses.
+You will recognize 4 corner Days
+or incur Easter Island Ending.
+
+Never a Genius knew Math
+to achieve my Cubic Wisdom.
+Cubic thought Reigns as the
+Highest Intelligence possible
+on the planet Earth. One 96
+hour rotating Cube within a
+single rotation of Earth -- is
+an Ineffable Transcendence.
+Bible and Science falsify 1
+corner day for the Cubic 4
+corner Days rotating daily.
+A single god is not possible
+in our 4 Day Cubic Science,
+that equates Cubic Divinity.
+Everybody is both stupid and
+evil for ignoring the 4 days.
+Cube Divinity transcends all
+knowledge, Humans can't
+escape 4 corner Cubic Life.
+Fools worship mechanics of
+language - while they wallow
+in fictitious & deceitful word.
+Exact science based on Cubics,
+not on theories. Wisdom is
+Cubic testing of knowledge.
+Academia is progression of
+Ignorance.   No god equals
+Simultaneous 4 Day Creation.
+Humans ignore their 4 corner
+stages of life metamorphosis.
+This site is a collection of data
+for a coming book - peruse it.
+No human has 2 hands as they
+are opposites, like plus and
+minus, that cancel as entity.
+Academia destroys your brain,
+your ability to think opposite.
+The eyes of the flounder fish
+were relocated, why were yours
+
+ relocated? Your opposite eyes
+were moved to 1 corner to overlay
+for single perspective, but that
+corrupts your Opposite Brain.
+*****************************
+
+KNOW CUBE, OR HELL.
+Education and Religion
+severely diminishes your
+intelligence and mentality,
+instituting ONEness Evil,
+You are educated stupid -
+and you have no inkling to
+just how EVIL you think.
+
+Seek in haste to attend a
+lecture by Dr. Gene Ray,
+Cubic and Wisest Human -
+His Wisdom is Awesome.
+*************************************
+For Cumming, GA 30041 Lecture,
+email: contact@2enp.com
+*******************************************
+Both Cubic Thinkers and SnotBrains were
+born with opposite brains, capable of math
+analysis to know most everything.    The
+Religious/Academic Oneness Brotherhood
+destroys the Brain's ability to think opposite
+of singularity trash taught.   Such reduced
+Brain intelligence begets the student a
+tag of SnotBrain android - encapable of
+comprehending absolute proof of 4 Days
+rotating simultaneously within a single
+rotation of Earth. Cubics comprehend it.
+You are a Cubic Thinker, or SnotBrain.
+You SnotBrains will know
+hell for ignoring TimeCube.
+I do not promote or suggest
+anyone killing you, but you
+are unfit to live on Earth.
+Binary Life Force is more
+Powerful than ONE God -
+especially the ole dead Jew,
+for which you've denounced
+your own mother and father.
+1 side brain can't reason
+without the opposite side.
+Americans are so dumb,
+educated stupid and evil,
+they have snot for brain.
+Believers have snot brain.
+Educated have snot brain.
+God worship only needs
+a snot brain, but it takes
+Opposite Brain Analysis
+to know Harmonic Life.
+The masculinity Sun and
+femininity Earth - form
+a Binary of Harmonic
+Opposites  at Center of
+Universe - Greater than
+either Sun or the Earth,
+debunking all fictitious
+Oneism Gods taught by
+religious/academic Word
+Animals. The invention of
+fictitious WORD inflicts
+humans as Word Animals
+- the dumbest and most
+Educated stupid and Evil
+animal to inhabit Earth
+and the Universe. Watch
+for Giant Erasor as there
+is a great danger of the
+human Word Animal being
+self-erased.   You cannot
+comprehend the actual 4
+
+ simultaneous days in single
+rotation of Earth, as 1 day
+1 God ONEism blocks the
+ability to think opposite of
+the ONEism crap taught.
+Education destroys brain.
+
+Dr. Gene Ray, Cubic and Wisest Human
+**********************************
+
+Gene Ray interview by Lionel on Air America Radio,
+The Lionel Show - 10am, Friday, August 3, 2007
+
+Creation is the Harmonics of Opposites -
+Opposites are the Harmonics of Creation.
+God entity is queer sex, or no opposite sex.
+God Oners must ban all sex with Opposites.
+Trinity of males degrade female opposites.
+Sex okay for atheist, but not God Oneists.
+Opposite hemispheres equate planets to a
+Giant Brain, that has 4 faces, but no limbs.
+Adults create baby, baby evolves to adult.
+No 1 God can create a planet of opposites,
+which equate to a zero value existence, and
+cancels to nothing as an entity in death. ')
 
 
 main()
